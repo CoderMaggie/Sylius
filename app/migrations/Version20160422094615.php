739c0ee1868c3c191bab0071d7a6198585357c9e
Magdalena Banasiak <magdalena.banasiak@lakion.com>
2016-04-22 11:47:28 +0200

    [CJMAX-46] Adjust fixtures

--- app/migrations/Version20160422094615.php
+++ app/migrations/Version20160422094615.php
@@ -8,7 +8,7 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version20160420122441 extends AbstractMigration
+class Version20160422094615 extends AbstractMigration
 {
     /**
      * @param Schema $schema
@@ -18,7 +18,8 @@ class Version20160420122441 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE CarouselItem (id INT AUTO_INCREMENT NOT NULL, path VARCHAR(255) DEFAULT NULL, tooltip VARCHAR(255) DEFAULT NULL, url VARCHAR(255) DEFAULT NULL, position INT NOT NULL, enabled TINYINT(1) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+        $this->addSql('DROP TABLE CarouselItem');
+        $this->addSql('ALTER TABLE carousel_item CHANGE position position INT DEFAULT NULL');
     }
 
     /**
@@ -29,6 +30,7 @@ class Version20160420122441 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('DROP TABLE CarouselItem');
+        $this->addSql('CREATE TABLE CarouselItem (id INT AUTO_INCREMENT NOT NULL, path VARCHAR(255) DEFAULT NULL COLLATE utf8_unicode_ci, tooltip VARCHAR(255) DEFAULT NULL COLLATE utf8_unicode_ci, url VARCHAR(255) DEFAULT NULL COLLATE utf8_unicode_ci, position INT NOT NULL, enabled TINYINT(1) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE carousel_item CHANGE position position INT NOT NULL');
     }
 }
--- src/AppBundle/Form/Type/CarouselItemType.php
+++ src/AppBundle/Form/Type/CarouselItemType.php
@@ -15,11 +15,12 @@ class CarouselItemType extends AbstractResourceType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('file', 'file', ['label' => 'app.ui.file'])
-                ->add('position', 'integer', ['label' => 'app.ui.position', 'required' => false])
-                ->add('tooltip', 'text', ['label' => 'app.ui.tooltip', 'required' => false])
-                ->add('url', 'url', ['label' => 'app.ui.url', 'required' => false])
-                ->add('enabled', 'checkbox', ['label' => 'sylius.ui.enabled'])
+        $builder
+            ->add('file', 'file', ['label' => 'app.ui.file'])
+            ->add('position', 'integer', ['label' => 'app.ui.position', 'required' => false])
+            ->add('tooltip', 'text', ['label' => 'app.ui.tooltip', 'required' => false])
+            ->add('url', 'url', ['label' => 'app.ui.url', 'required' => false])
+            ->add('enabled', 'checkbox', ['label' => 'sylius.ui.enabled'])
         ;
     }
 
