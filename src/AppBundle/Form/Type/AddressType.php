f9094d579c02b42107e5b096a4accb2030aeef18
Magdalena Banasiak <magdalena.banasiak@lakion.com>
2016-05-11 11:05:26 +0200

    [CJMAX-84] Preselect UK as shipping country in checkout

---
+++ src/AppBundle/Form/Type/AddressType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace AppBundle\Form\Type;
+
+use Sylius\Bundle\AddressingBundle\Form\Type\AddressType as BaseAddressType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+/**
+ * @author Magdalena Banasiak <magdalena.banasiak@lakion.com>
+ */
+class AddressType extends BaseAddressType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        parent::buildForm($builder, $options);
+
+        $builder
+            ->add('countryCode', 'sylius_country_code_choice', [
+                'label' => 'sylius.form.address.country',
+                'enabled' => true,
+                'data' => 'GB'
+            ])
+        ;
+    }
+}
